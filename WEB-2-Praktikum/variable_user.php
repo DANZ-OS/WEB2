010087393d0185f455cb92406eeb3d4477f061d8
DANZ-OS <zaidaandzn@gmail.com>
2025-02-20 12:10:07 +0700

    Add files via upload

---
+++ WEB-2-Praktikum/variable_user.php
@@ -0,0 +1,11 @@
+<?php
+//mendefinisikan variable
+$nama = "Zaidaan";
+$umur = 18;
+$beratbadan = 55.0;
+
+echo 'Nama saya' . $nama;
+echo '<br/>Umur saya' . $umur;
+echo '<br/>dan berat badan saya' . $beratbadan;
+
+?>
