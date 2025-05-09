4a2c178fa052a32de3f8112552868c5a1b007688
DANZ-OS <zaidaandzn@gmail.com>
2025-05-09 23:22:59 +0700

    praktikum07

--- my-app/database/seeders/DatabaseSeeder.php
+++ my-app/database/seeders/DatabaseSeeder.php
@@ -15,16 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        $this->call([
-            RuangSeeder::class,
-            UnitKerjaSeeder::class,
-            PegawaiSeeder::class,
-            PeminjamanSeeder::class,
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
