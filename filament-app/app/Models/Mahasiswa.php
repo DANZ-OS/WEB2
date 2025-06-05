a6b6f44a6cb6a19d61cc3bbe655f29ebfa523e44
DANZ-OS <zaidaandzn@gmail.com>
2025-06-05 11:08:11 +0700

    Membuat fitur create

--- filament-app/app/Models/Mahasiswa.php
+++ filament-app/app/Models/Mahasiswa.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
-    //
+    protected $fillable = ['nama', 'nim', 'email', 'telepon', 'alamat', 'tanggal_lahir', 'jurusan', 'foto', 'status', 'angkatan', 'jenis_kelamin', 'agama'];
 }
